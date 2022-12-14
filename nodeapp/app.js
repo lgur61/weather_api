e4a9ffdc3abe6b6eb8527275e4e8aba9ad205dc4
LG <9917035+lgur61@users.noreply.github.com>
2022-12-15 02:01:50 +0300

    app

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,73 +1,78 @@
-const express = require("express")
-const axios = require("axios")
+const express = require("express");
+const axios = require("axios");
 const redis = require("redis");
-/*
+
 const redisClient = redis.createClient({
-  host: 'production-ecs-cluster-redis.atppn3.0001.use1.cache.amazonaws.com',
-  port: 6379, // default port 
-});
-*/
-const redisClient = redis.createClient({
-    socket: {
-        host: 'production-ecs-cluster-redis.atppn3.0001.use1.cache.amazonaws.com',
-        port: 6379
-    }   
+  socket: {
+    host: "production-ecs-cluster-redis.atppn3.0001.use1.cache.amazonaws.com",
+    port: 6379,
+  },
 });
 
-const app = express()
+(async () => {
+  await redisClient.connect();
+})();
 
+const app = express();
 
+redisClient.on("ready", () => {
+  console.log("Connected!");
+});
+
+redisClient.on("error", (err) => {
+  console.error(err);
+});
 
 const cache = (req, res, next) => {
   const { location } = req.params;
 
-  redisClient.get(location, (error, result) => {
-    if (error) next(error);
-    if (result !== null) {
-      let res = JSON.parse(result)
-      //res.source = "Redis Cache";
-      return res.json(res);
-    } else {
-      return next();
-    }
-  });
+  redisClient
+    .get(location)
+    .then((value) => {
+      if (value !== null) {
+        let val = JSON.parse(value);
+        val.source = "Redis Cache";
+        return res.json(val);
+      } else {
+        return next();
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 };
 
-app.use((err,req,res,next)=> {
-     err.statusCode= err.statusCode || 500
-     err.status= err.status || 'error'
-     res.status(err.statusCode).json({
-          status:err.status,
-          message:err.message
-     })
-})
-app.get("/current/:location", cache, async function(req, res) {
-    const API_KEY = "aa1b2c2361b74ac8c919ab868e0b7f1b"
-  try {   
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
+app.get("/current/:location", cache, async function (req, res) {
+  const API_KEY = "aa1b2c2361b74ac8c919ab868e0b7f1b";
+  const { location } = req.params;
+  try {
     const coordinates = await axios.get(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${req.params.location}&limit=3&appid=${API_KEY}`    
-    ); 
-    
-    const weather = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.data[0].lat}&lon=${coordinates.data[0].lon}&appid=${API_KEY}`    
-    );    
-   
-    redisClient.set(req.params.location, JSON.stringify(weather.data))
-    weather.data.source = "Weather API"
-    return res.json(weather.data);
+      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=3&appid=${API_KEY}`
+    );
 
-      
+    const weather = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.data[0].lat}&lon=${coordinates.data[0].lon}&appid=${API_KEY}`
+    );
+
+    redisClient.set(location, JSON.stringify(weather.data));
+    weather.data.source = "Weather API";
+    return res.json(weather.data);
   } catch (error) {
-    res.status(500).send({error: error.message});
+    throw error;
   }
 });
 
-app.get("/health", async function(req, res) {   
-    res.status(200).send("Service is up and running !");  
+app.get("/health", async function (req, res) {
+  res.status(200).send("Service is up and running !");
 });
 
-
-
-
-app.listen(8080, () => console.log(`Running on 8080`))
+app.listen(8080, () => console.log(`Running on 8080`));
